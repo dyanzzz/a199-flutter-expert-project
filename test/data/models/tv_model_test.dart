d251d9523b0da555e9e7bd4383b7caed36a161e9
Dian Tri Budianto <dyanzzz@gmail.com>
2021-12-31 23:00:24 +0700

    Implement Test Entity & Model Data TV Show

---
+++ test/data/models/tv_model_test.dart
@@ -0,0 +1,42 @@
+import 'package:ditonton/data/models/tv_model.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final testTvModel = TvModel(
+    backdropPath: 'backdropPath',
+    firstAirDate: DateTime.parse('2021-11-24'),
+    genreIds: [1, 2, 3],
+    id: 1,
+    name: 'name',
+    originCountry: ['originCountry'],
+    originalLanguage: 'originalLanguage',
+    originalName: 'originalName',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  final testTv = Tv(
+    backdropPath: 'backdropPath',
+    firstAirDate: DateTime.parse('2021-11-24'),
+    genreIds: [1, 2, 3],
+    id: 1,
+    name: 'name',
+    originCountry: ['originCountry'],
+    originalLanguage: 'originalLanguage',
+    originalName: 'originalName',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  test('should be a subclass of TV entity', () async {
+    final result = testTvModel.toEntity();
+    expect(result, testTv);
+  });
+}
