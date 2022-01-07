e3ee45417742214207fe4ba61aa33b570865074b
Dian Tri Budianto <dyanzzz@gmail.com>
2022-01-07 22:13:26 +0700

    Add Provider Notifier Tv Show Top Rated Implement TDD Provider Notifier Tv Show Top Rated

---
+++ test/presentation/provider/tv_top_rated_test.dart
@@ -0,0 +1,78 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:ditonton/domain/usecases/get_tv_top_rated.dart';
+import 'package:ditonton/presentation/provider/tv_top_rated_notifier.dart.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'tv_list_notifier_test.mocks.dart';
+
+@GenerateMocks([GetTvTopRated])
+void main() {
+  late MockGetTvTopRated mockGetTvTopRated;
+  late TvTopRatedNotifier notifier;
+  late int listenerCallCount;
+
+  setUp(() {
+    listenerCallCount = 0;
+    mockGetTvTopRated = MockGetTvTopRated();
+    notifier = TvTopRatedNotifier(getTvTopRated: mockGetTvTopRated)
+      ..addListener(() {
+        listenerCallCount++;
+      });
+  });
+
+  final tTv = Tv(
+    backdropPath: '/1R68vl3d5s86JsS2NPjl8UoMqIS.jpg',
+    genreIds: [14, 28],
+    id: 88329,
+    originalName: 'Hawkeye',
+    overview:
+        'Former Avenger Clint Barton has a seemingly simple mission: get back to his family for Christmas. Possible? Maybe with the help of Kate Bishop, a 22-year-old archer with dreams of becoming a superhero. The two are forced to work together when a presence from Barton’s past threatens to derail far more than the festive spirit.',
+    popularity: 1607.746,
+    posterPath: '/pqzjCxPVc9TkVgGRWeAoMmyqkZV.jpg',
+    name: 'Hawkeye',
+    voteAverage: 8.5,
+    voteCount: 1267,
+    firstAirDate: DateTime.parse('2021-11-24'),
+    originalLanguage: 'en',
+    originCountry: ['US'],
+  );
+  final tTvList = <Tv>[tTv];
+
+  test('should change state to loading when usecase is called', () async {
+    // arrange
+    when(mockGetTvTopRated.execute()).thenAnswer((_) async => Right(tTvList));
+    // act
+    notifier.fetchTvTopRated();
+    // assert
+    expect(notifier.state, RequestState.Loading);
+    expect(listenerCallCount, 1);
+  });
+
+  test('should change tv data when data is gotten successfully', () async {
+    // arrange
+    when(mockGetTvTopRated.execute()).thenAnswer((_) async => Right(tTvList));
+    // act
+    await notifier.fetchTvTopRated();
+    // assert
+    expect(notifier.state, RequestState.Loaded);
+    expect(notifier.tv, tTvList);
+    expect(listenerCallCount, 2);
+  });
+
+  test('should return error when data is unsuccessful', () async {
+    // arrange
+    when(mockGetTvTopRated.execute())
+        .thenAnswer((_) async => Left(ServerFailure('Server Failure')));
+    // act
+    await notifier.fetchTvTopRated();
+    // assert
+    expect(notifier.state, RequestState.Error);
+    expect(notifier.message, 'Server Failure');
+    expect(listenerCallCount, 2);
+  });
+}
