af83bf52b3ebf44d1a6c60d6d91720c39b5d4711
Dian Tri Budianto <dyanzzz@gmail.com>
2022-01-08 23:36:26 +0700

    Add Widget Page & Card Tv Show Top Rated Implement TDD Check Widget Page Tv Show Top Rated

---
+++ test/presentation/pages/tv_top_rated_page_test.dart
@@ -0,0 +1,66 @@
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:ditonton/presentation/pages/tv_top_rated_page.dart';
+import 'package:ditonton/presentation/provider/tv_top_rated_notifier.dart.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
+
+import 'tv_top_rated_page_test.mocks.dart';
+
+@GenerateMocks([TvTopRatedNotifier])
+void main() {
+  late MockTvTopRatedNotifier mockTvTopRatedNotifier;
+
+  setUp(() {
+    mockTvTopRatedNotifier = MockTvTopRatedNotifier();
+  });
+
+  Widget _makeTestableWidget(Widget body) {
+    return ChangeNotifierProvider<TvTopRatedNotifier>.value(
+      value: mockTvTopRatedNotifier,
+      child: MaterialApp(
+        home: body,
+      ),
+    );
+  }
+
+  testWidgets('page should display progress bar when loading',
+      (WidgetTester tester) async {
+    when(mockTvTopRatedNotifier.state).thenReturn(RequestState.Loading);
+
+    final progressFinder = find.byType(CircularProgressIndicator);
+    final centerFinder = find.byType(Center);
+
+    await tester.pumpWidget(_makeTestableWidget(TvTopRatedPage()));
+
+    expect(centerFinder, findsOneWidget);
+    expect(progressFinder, findsOneWidget);
+  });
+
+  testWidgets('page should display when data is loaded',
+      (WidgetTester tester) async {
+    when(mockTvTopRatedNotifier.state).thenReturn(RequestState.Loaded);
+    when(mockTvTopRatedNotifier.tv).thenReturn(<Tv>[]);
+
+    final listViewFinder = find.byType(ListView);
+
+    await tester.pumpWidget(_makeTestableWidget(TvTopRatedPage()));
+
+    expect(listViewFinder, findsOneWidget);
+  });
+
+  testWidgets('page should display text with message when error',
+      (WidgetTester tester) async {
+    when(mockTvTopRatedNotifier.state).thenReturn(RequestState.Error);
+    when(mockTvTopRatedNotifier.message).thenReturn('Error message');
+
+    final textFinder = find.byKey(Key('error_message'));
+
+    await tester.pumpWidget(_makeTestableWidget(TvTopRatedPage()));
+
+    expect(textFinder, findsOneWidget);
+  });
+}
