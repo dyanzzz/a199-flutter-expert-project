aafc9d0e2888236dac827d68a003a61735ceb510
Dian Tri Budianto <dyanzzz@gmail.com>
2022-01-21 14:08:12 +0700

    Implement spend time when write keyword to query search film & tv show

--- search/lib/bloc/search_bloc.dart
+++ search/lib/bloc/search_bloc.dart
@@ -1,9 +1,8 @@
 import 'package:core/core.dart';
-import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:equatable/equatable.dart';
 import 'package:search/search.dart';
+import 'package:rxdart/rxdart.dart';
 
 part 'search_event.dart';
 part 'search_state.dart';
@@ -11,22 +10,30 @@ part 'search_state.dart';
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
   final SearchMovies _searchMovies;
 
+  EventTransformer<T> debounce<T>(Duration duration) {
+    return (events, mapper) => events.debounceTime(duration).flatMap(mapper);
+  }
+
   SearchBloc(this._searchMovies) : super(SearchEmpty()) {
-    on<OnQueryChanged>((event, emit) async {
-      final query = event.query;
+    on<OnQueryChanged>(
+      (event, emit) async {
+        final query = event.query;
+        print(query);
 
-      emit(SearchLoading());
+        emit(SearchLoading());
 
-      final result = await _searchMovies.execute(query);
+        final result = await _searchMovies.execute(query);
 
-      result.fold(
-        (failure) {
-          emit(SearchError(failure.message));
-        },
-        (data) {
-          emit(SearchMovieHasData(data));
-        },
-      );
-    });
+        result.fold(
+          (failure) {
+            emit(SearchError(failure.message));
+          },
+          (data) {
+            emit(SearchMovieHasData(data));
+          },
+        );
+      },
+      transformer: debounce(const Duration(milliseconds: 500)),
+    );
   }
 }
--- search/lib/bloc/search_tv_bloc.dart
+++ search/lib/bloc/search_tv_bloc.dart
@@ -1,26 +1,35 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:search/bloc/search_bloc.dart';
 import 'package:search/search.dart';
+import 'package:rxdart/rxdart.dart';
 
 class SearchTvBloc extends Bloc<SearchEvent, SearchState> {
   final SearchTv _searchTv;
 
+  EventTransformer<T> debounce<T>(Duration duration) {
+    return (events, mapper) => events.debounceTime(duration).flatMap(mapper);
+  }
+
   SearchTvBloc(this._searchTv) : super(SearchEmpty()) {
-    on<OnQueryChanged>((event, emit) async {
-      final query = event.query;
+    on<OnQueryChanged>(
+      (event, emit) async {
+        final query = event.query;
+        print(query);
 
-      emit(SearchLoading());
+        emit(SearchLoading());
 
-      final result = await _searchTv.execute(query);
+        final result = await _searchTv.execute(query);
 
-      result.fold(
-        (failure) {
-          emit(SearchError(failure.message));
-        },
-        (data) {
-          emit(SearchTvHasData(data));
-        },
-      );
-    });
+        result.fold(
+          (failure) {
+            emit(SearchError(failure.message));
+          },
+          (data) {
+            emit(SearchTvHasData(data));
+          },
+        );
+      },
+      transformer: debounce(const Duration(milliseconds: 500)),
+    );
   }
 }
