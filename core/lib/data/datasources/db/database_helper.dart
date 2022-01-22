4deecab79d85f3373e7b7dac6ba8a5ac27c10a17
Dian Tri Budianto <dyanzzz@gmail.com>
2022-01-22 07:42:31 +0700

    Implement sqlchipper and encrypt database_helper

--- core/lib/data/datasources/db/database_helper.dart
+++ core/lib/data/datasources/db/database_helper.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
+import 'package:core/common/encrypt.dart';
 import 'package:core/core.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_sqlcipher/sqflite.dart';
 
 class DatabaseHelper {
   static DatabaseHelper? _databaseHelper;
@@ -28,7 +29,12 @@ class DatabaseHelper {
     final path = await getDatabasesPath();
     final databasePath = '$path/ditonton.db';
 
-    var db = await openDatabase(databasePath, version: 1, onCreate: _onCreate);
+    var db = await openDatabase(
+      databasePath,
+      version: 1,
+      onCreate: _onCreate,
+      password: encrypt("admin"),
+    );
     return db;
   }
 
